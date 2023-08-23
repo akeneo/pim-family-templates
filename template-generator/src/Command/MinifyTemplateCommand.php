e2f988187001a319e2a8d9a3b02bc4a9500cbe8a
Grégoire Houssard <ghoussard@protonmail.com>
2023-08-23 13:52:30 +0200

    RAB-1495: Add command to minify template files

---
+++ template-generator/src/Command/MinifyTemplateCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\TemplateGenerator\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MinifyTemplateCommand extends Command
+{
+    protected static $defaultName = 'template-generator:template:minify';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Minify JSON template files')
+            ->addArgument('template_directory', InputArgument::REQUIRED, 'Templates directory to minify')
+            ->addArgument('output_file', InputArgument::REQUIRED, 'Output file path');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $templatesDirectory = $input->getArgument('template_directory');
+
+        $industries = $this->readIndustries($templatesDirectory);
+        $familyTemplates = $this->readFamilyTemplates($templatesDirectory);
+        $attributeOptions = $this->readAttributeOptions($templatesDirectory);
+
+        $outputFile = $input->getArgument('output_file');
+        file_put_contents($outputFile, json_encode([
+            'industries' => $industries,
+            'family_templates' => $familyTemplates,
+            'attribute_options' => $attributeOptions,
+        ]));
+
+        return Command::SUCCESS;
+    }
+
+    private function readIndustries(string $templatesDirectory): array
+    {
+        $industriesFilePath = $this->getFilePath($templatesDirectory, 'industries');
+
+        return $this->readJsonFile($industriesFilePath);
+    }
+
+    private function readFamilyTemplates(string $templatesDirectory): array
+    {
+        $familyTemplatesDirectory = sprintf('%s/%s', $templatesDirectory, 'families');
+        $familyTemplateFiles = array_filter(scandir($familyTemplatesDirectory), static fn (string $file) => !in_array($file, ['.', '..']));
+
+        $familyTemplates = [];
+
+        foreach ($familyTemplateFiles as $familyTemplateFile) {
+            $familyTemplateFilePath = sprintf('%s/%s', $familyTemplatesDirectory, $familyTemplateFile);
+            $familyTemplate = $this->readJsonFile($familyTemplateFilePath);
+            $familyTemplates[$familyTemplate['code']] = $familyTemplate;
+        }
+
+        return $familyTemplates;
+    }
+
+    private function readAttributeOptions(string $templatesDirectory): array
+    {
+        $attributeOptionsFilePath = $this->getFilePath($templatesDirectory, 'attribute_options');
+
+        return $this->readJsonFile($attributeOptionsFilePath);
+    }
+
+    private function getFilePath(string $templatesDirectory, string $fileNameWithoutExtension): string
+    {
+        return sprintf('%s/%s.json', $templatesDirectory, $fileNameWithoutExtension);
+    }
+
+    private function readJsonFile(string $filePath): array
+    {
+        $json = file_get_contents($filePath);
+
+        return json_decode(json: $json, flags: JSON_OBJECT_AS_ARRAY);
+    }
+}
