0304776f4cdadfec01ee33b2a37ce319d515a4f6
Grégoire Houssard <ghoussard@protonmail.com>
2023-12-05 17:06:43 +0100

    RAB-1744: Fix reviews

--- src/Command/CalculateMetricsCommand.php
+++ src/Command/CalculateMetricsCommand.php
@@ -4,6 +4,7 @@ namespace Akeneo\PimFamilyTemplates\Command;
 
 use Google\Cloud\BigQuery\BigQueryClient;
 use JiraRestApi\Board\BoardService;
+use JiraRestApi\Issue\AgileIssue;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,9 +15,8 @@ class CalculateMetricsCommand extends Command
     private const SERVICES_JIRA_BOARD_ID = 74;
     private const SERVICES_JIRA_MAX_RESULTS = 500;
     private const SF_PROJECT_ID_JIRA_CUSTOM_FIELD = 'customfield_13563';
-    private const KICK_OFF_DATE_JIRA_CUSTOM_FIELD = 'customfield_13564';
     private const GCP_LOCATION = 'eu';
-    private const GCP_PROJECT_ID = 'akecld-prd-pim-saas-prod';
+    private const GCP_PROD_PROJECT_ID = 'akecld-prd-pim-saas-prod';
 
     protected static $defaultName = 'metrics:calculate';
 
@@ -36,18 +36,17 @@ class CalculateMetricsCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $customers = $this->findSaasCustomersOlderLessThan6MonthsFromJira();
-        $output->writeln(sprintf('Fetched %d SaaS customers which have kickoff date older less than 6 months from Jira', count($customers)));
+        $projectIds = $this->findProjectIdsOlderLessThan6MonthsFromJira();
+        $output->writeln(sprintf('Fetched %d project ids which have kickoff date older less than 6 months from Jira', count($projectIds)));
 
-        $projectIds = array_keys($customers);
-        $customersInstances = $this->findCustomersInstancesFromBigQuery($projectIds);
-        $output->writeln(sprintf('Fetched %d (out of %d) customers instances from BigQuery', count($customersInstances), count($customers)));
+        $tenantIdsByProjectIds = $this->findTenantIdsByProjectIdsFromBigQuery($projectIds);
+        $output->writeln(sprintf('Fetched %d (out of %d) project ids which have tenant id from BigQuery', count($tenantIdsByProjectIds), count($projectIds)));
 
-        $customersWhoUsedFeature = $this->findCustomersWhoUsedFeatureFromBigQuery($customersInstances);
-        $output->writeln(sprintf('Found %d (out of %d) customers who used the feature from BigQuery', count($customersWhoUsedFeature), count($customers)));
+        $projectIdsWhoUsedFeature = $this->findProjectIdsWhoUsedFeatureFromBigQuery($tenantIdsByProjectIds);
+        $output->writeln(sprintf('Found %d (out of %d) project ids which used the feature from BigQuery', count($projectIdsWhoUsedFeature), count($projectIds)));
 
-        $metric = count($customersWhoUsedFeature) / count($customers) * 100;
-        $output->writeln(sprintf('Metric 1: %f percents of new customers (6 months) has used Family Template feature during the last two weeks', $metric));
+        $metric = count($projectIdsWhoUsedFeature) / count($projectIds) * 100;
+        $output->writeln(sprintf('Metric 1: %f percents of new customers (6 months) has used Family Template feature', $metric));
 
         return Command::SUCCESS;
     }
@@ -55,7 +54,7 @@ class CalculateMetricsCommand extends Command
     /**
      * @return array<string, array{sf_project_id: string, kick_off_date: \DateTimeImmutable}>
      */
-    private function findSaasCustomersOlderLessThan6MonthsFromJira(): array
+    private function findProjectIdsOlderLessThan6MonthsFromJira(): array
     {
         $jql = '"SF Project ID[Short text]" is not empty AND "Project Kick Off Date[Date]" > startOfDay(-6M)';
 
@@ -64,35 +63,27 @@ class CalculateMetricsCommand extends Command
             'maxResults' => self::SERVICES_JIRA_MAX_RESULTS,
         ]);
 
-        if (self::SERVICES_JIRA_MAX_RESULTS <= count($issues)) {
+        if (self::SERVICES_JIRA_MAX_RESULTS <= $issues->count()) {
             throw new \RuntimeException('Max results reached on Jira API. Increase it or implement pagination.');
         }
 
-        $customers = [];
-        foreach ($issues as $issue) {
-            $sFProjectId = $issue->fields->customFields[self::SF_PROJECT_ID_JIRA_CUSTOM_FIELD];
-            $kickOffDate = new \DateTimeImmutable($issue->fields->customFields[self::KICK_OFF_DATE_JIRA_CUSTOM_FIELD]);
-
-            $customers[$sFProjectId] = [
-                'sf_project_id' => $sFProjectId,
-                'kick_off_date' => $kickOffDate,
-            ];
-        }
-
-        return $customers;
+        return array_map(
+            static fn (AgileIssue $issue) => $issue->fields->customFields[self::SF_PROJECT_ID_JIRA_CUSTOM_FIELD],
+            $issues->getArrayCopy(),
+        );
     }
 
-    private function findCustomersInstancesFromBigQuery(array $projectIds): array
+    private function findTenantIdsByProjectIdsFromBigQuery(array $projectIds): array
     {
         $sql = <<<SQL
 SELECT
   sf_project.project_record_id as sf_project_id,
-  ARRAY_AGG(JSON_OBJECT(['type', 'tenant_id'], [papo_product.environment, papo_product.instance_name])) as instances
+  ARRAY_AGG(papo_product.instance_name IGNORE NULLS) as tenant_ids
 FROM
   `ake-actionable-product-data.pim_customers_data.sf_project` sf_project
 JOIN `ake-actionable-product-data.pim_customers_data.papo_product` papo_product ON sf_project.partners_portal_project_id = papo_product.project_id
 WHERE
-  sf_project.pim_version = 'SaaS' AND sf_project.project_record_id IN (%s) AND papo_product.instance_name IS NOT NULL
+  sf_project.pim_version = 'SaaS' AND sf_project.project_record_id IN (%s)
 GROUP BY sf_project.project_record_id;
 SQL;
         $sqlProjectIds = join(', ', array_map(static fn (string $projectId) => sprintf("'%s'", $projectId), $projectIds));
@@ -100,7 +91,7 @@ SQL;
         $process = new Process([
             'bq',
             sprintf('--location=%s', self::GCP_LOCATION),
-            sprintf('--project_id=%s', self::GCP_PROJECT_ID),
+            sprintf('--project_id=%s', self::GCP_PROD_PROJECT_ID),
             'query',
             '--format=json',
             '--use_legacy_sql=false',
@@ -114,51 +105,40 @@ SQL;
             throw new \RuntimeException(sprintf('Unable to fetch customer instances from BigQuery. Error output: %s', $process->getErrorOutput()));
         }
 
-        $customersInstances = [];
+        $tenantIdsByProjectIds = [];
         foreach ($rows as $row) {
-            $customersInstances[$row['sf_project_id']] = array_map(
-                static fn (string $rawInstances) => json_decode(json: $rawInstances, associative: true, flags: JSON_OBJECT_AS_ARRAY),
-                $row['instances'],
-            );
+            $tenantIdsByProjectIds[$row['sf_project_id']] = $row['tenant_ids'];
         }
 
-        return $customersInstances;
+        return $tenantIdsByProjectIds;
     }
 
-    private function findCustomersWhoUsedFeatureFromBigQuery(array $customersInstances): array
+    private function findProjectIdsWhoUsedFeatureFromBigQuery(array $tenantIdsByProjectIds): array
     {
-        $tenantIdsByProjectId = array_map(
-            static fn (array $instances) => array_map(
-                static fn (array $instance) => $instance['tenant_id'],
-                $instances,
-            ),
-            $customersInstances,
-        );
-
         $sql = <<<SQL
 SELECT DISTINCT(tenant_id)
 FROM `akecld-saas-dev.raccoons.create_family_from_template_usage`
-WHERE tenant_id IN UNNEST(?);
+WHERE tenant_id IN UNNEST(?) AND gcp_project_id = ?;
 SQL;
 
         $tenantIds = array_reduce(
-            $tenantIdsByProjectId,
-            static fn (array $tenantIds, array $currentTenantIds) => [...$tenantIds, ...$currentTenantIds],
+            $tenantIdsByProjectIds,
+            static fn (array $accumulator, array $tenantIds) => [...$accumulator, ...$tenantIds],
             [],
         );
 
-        $query = $this->bigQueryClient->query($sql)->parameters([$tenantIds]);
+        $query = $this->bigQueryClient->query($sql)->parameters([$tenantIds, self::GCP_PROD_PROJECT_ID]);
         $results = $this->bigQueryClient->runQuery($query);
         $results->waitUntilComplete();
 
         $tenantsIdsWhoUsedFeature = array_reduce(
             iterator_to_array($results),
-            static fn ($tenantIds, $result) => [...$tenantIds, $result['tenant_id']],
+            static fn ($accumulator, $result) => [...$accumulator, $result['tenant_id']],
             [],
         );
 
         return array_keys(array_filter(
-            $tenantIdsByProjectId,
+            $tenantIdsByProjectIds,
             static fn (array $tenantIds) => 0 < count(array_intersect($tenantsIdsWhoUsedFeature, $tenantIds)),
         ));
     }
